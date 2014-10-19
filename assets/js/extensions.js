e655cbf26a42bacf48944854b10faa1ce81825b1
Rufus Pollock <rufus.pollock@okfn.org>
2014-10-19 18:57:52 +0100

    [#4,all][l]: first pass implementation of extensions registry with basic stuff working.
    What is working:
    
    * Basic jekyll site with CKAN theme working (copied from
      https://github.com/ckan/ckan.github.io and then cleaned up and stripped down)
      * fixes #3 - theme site
    * Home page listing of all extensions based on okfnlabs.org/project page
      * fixes #2 - basic home page (though not really finished)
    * One example extension (with frontmatter) in extensions/geo.md
    
    What is not:
    
    * Have not written all extensions to extensions/ directory - so not really listing extensions yet!
    * Have not really modded the projects listing code so e.g. filter not yet set up for our case

---
+++ assets/js/extensions.js
@@ -0,0 +1,78 @@
+jQuery(document).ready(function($) {
+  var container = $('.extensions');
+  var chosen_select = $('.chosen-select');
+
+  $(".form-inline").removeClass("hidden");
+
+  // triggered when the form text is changed
+  $("form#filters").on('change', function() {
+    var chosen_opts = $('.chosen-select').val();
+    if (chosen_opts !== null) {
+      chosen_opts = chosen_opts.join('');
+    }
+    // filter extensions
+    container.isotope({
+      filter: chosen_opts
+    });
+
+    return false;
+  });
+
+  var filters = {tags: {}, type: {}, status: {}, language: {}};
+
+  container.find('.record').each(function() {
+    // Find the current extension's detail URL
+    var record = $(this);
+    var url = record.data('url');
+    record.addClass('expand');
+
+    // de-duplicate filter values
+    $.each(filters, function (filter_type) {
+      var filter_vals = record.data(filter_type).split(';');
+      $.each(filter_vals, function(ignore, filter_val) {
+        k = toFilterName(filter_val);
+        if (!(k in filters[filter_type]) && k !== '') {
+          filters[filter_type][k] = filter_val;
+        }
+      });
+    });
+
+    // Mouse over the record container
+    record.hover(function() {
+      var record = $(this);
+      if (!record.hasClass('loaded')) {
+        // Load and display it on hover
+        $.get(url, function(data) {
+          record.append(
+            $(data).find('.record .rhs').html()
+          ).addClass('loaded');
+        });
+      }
+    });
+  });
+
+  // create filter options
+  $.each(filters, function(filter_type, filter) {
+    var chosen_group = $('<optgroup label="' + filter_type.substr(0,1).toUpperCase() + filter_type.substr(1) + '">');
+    $.each(filter, function(k, v) {
+      // the '*=' here is a hack. if a filter value is a substring of
+      // another (e.g. java and javascript) this will go wrong.
+      chosen_group.append('<option value="[data-' + filter_type + '*=' + v + ']">' + v + '</option>');
+    });
+    chosen_select.append(chosen_group);
+  });
+
+  // Parse filter title
+  function toFilterName(title) {
+    return title.trim().toLowerCase();
+  }
+
+  // Create Isotope grid view
+  container.isotope({
+    itemSelector: '.record',
+    layoutMode: 'masonry',
+    filter: '[data-featured=true]'
+  });
+
+  $('.chosen-select').chosen();
+});
