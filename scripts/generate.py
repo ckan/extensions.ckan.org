8521321ef7cdb84db46f1cc292a0136d94719e7a
amercader <amercadero@gmail.com>
2021-08-26 15:53:26 +0200

    Generate script: py3, more robust

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,17 +1,19 @@
 import csv
 import os
-import urllib
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+import codecs
 import pypandoc
 from collections import OrderedDict
 
-class AttrDict(dict): 
+class AttrDict(dict):
     __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
 
 
 def get_master(master_csv_url):
-    fo = urllib.urlopen(master_csv_url)
-    reader = csv.DictReader(fo)
+    fo = urlopen(master_csv_url)
+    reader = csv.DictReader(codecs.iterdecode(fo, 'utf-8'))
     return [ AttrDict(x) for x in reader ]
 
 # assumes on github
@@ -22,10 +24,19 @@ def get_extension_info(master_info_dict):
     outdict.title = outdict.title.split('.')[0].replace(':', '-')
     # url is github url
     parts = outdict.url.split('/')
-    outdict['github_user'] = parts[3]
-    outdict['github_repo'] = parts[4]
+    if len(parts) == 5:
+        outdict['github_user'] = parts[3]
+        outdict['github_repo'] = parts[4]
+    else:
+        outdict['github_user'] = ''
+        outdict['github_repo'] = ''
+
     # try to find the README or README.md etc
-    outdict.readme = _get_readme(outdict.github_user, outdict.github_repo)
+    try:
+        outdict.readme = _get_readme(outdict.github_user, outdict.github_repo)
+    except (HTTPError, URLError):
+        print(f'Error getting README for {outdict.name}')
+        outdict['readme'] = '<Error getting README>'
     return outdict
 
 def _get_readme(user, repo):
@@ -33,20 +44,20 @@ def _get_readme(user, repo):
     for ext in exts:
         urlbase = 'https://raw.githubusercontent.com/%s/%s/master/' % (user, repo)
         url = urlbase + 'README' + ext
-        urlfo = urllib.urlopen(url)
+        urlfo = urlopen(url, timeout=5)
         if urlfo.getcode() == 404:
             # try lowercase readme
             url = urlbase + 'readme' + ext
-            urlfo = urllib.urlopen(url)
+            urlfo = urlopen(url)
         if urlfo.getcode() != 404:
             readme = urlfo.read()
             # {% is reserved for liquid templates
-            readme = readme.replace('{% ', '{%raw%}{% {%endraw%}')
-            readme = readme.replace(' %}', '{%raw%} %}{%endraw%}')
+            readme = readme.replace(b'{% ', b'{%raw%}{% {%endraw%}')
+            readme = readme.replace(b' %}', b'{%raw%} %}{%endraw%}')
             if ext == '.rst':
-                readme = pypandoc.convert(readme, to='markdown_github', format = 'rst' )
+                readme = pypandoc.convert_text(readme, to='markdown_github', format = 'rst' )
             else:
-                readme = pypandoc.convert(readme, to='markdown_github', format = 'markdown_github' ) 
+                readme = pypandoc.convert_text(readme, to='markdown_github', format = 'markdown_github' )
             return readme
 
 def write_extension(extension):
@@ -67,8 +78,8 @@ permalink: /extension/%(name)s/
 ---
 ''' % extension
 
-    if type(extension.readme) == unicode:
-        extension.readme = extension.readme.encode('ascii', 'xmlcharrefreplace')
+#    if type(extension.readme) == unicode:
+#        extension.readme = extension.readme.encode('ascii', 'xmlcharrefreplace')
     out = '%s\n\n%s\n' % (frontmatter, extension.readme)
     outfo.write(out)
     outfo.close()
